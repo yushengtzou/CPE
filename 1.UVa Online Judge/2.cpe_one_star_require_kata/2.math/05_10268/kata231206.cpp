0884e00efc0601856ffe403db766c1c4b3c5e028
Yu-Sheng Tzou <daniel.tzou1021@gmail.com>
2023-12-07 20:53:44 +0800

    Update 231207

--- 1.UVa Online Judge/2.cpe_one_star_require_kata/2.math/05_10268/kata231206.cpp
+++ 1.UVa Online Judge/2.cpe_one_star_require_kata/2.math/05_10268/kata231206.cpp
@@ -10,14 +10,15 @@ using namespace std;
 
 
 // 計算多項式在給定點 x 的導數的函式
-int calculateDerivative(int x, const vector<int>& coefficients) {
+int derivative(int x, const vector<int>& coeffs) {
     long long sum = 0;  // 多項式導數的總和
-    long long powerOfX = 1;  // 用來追蹤 x 的遞增次方
+    long long exp = 1;  // 用來追蹤 x 的遞增次方
+    int max = coeffs.size() - 1;
 
     // 從次高項開始（因為導數會降低一個次數）
-    for (int i = coefficients.size() - 2; i >= 0; --i) {
-        sum += coefficients[i] * powerOfX * (coefficients.size() - 1 - i);
-        powerOfX *= x;
+    for (int i = max - 1; i >= 0; i--) {
+        sum += coeffs[i] * exp * (max - i);
+        exp *= x;
     }
     return sum;
 }
@@ -26,18 +27,18 @@ int main() {
     int x;  // 計算導數的點
 
     while(cin >> x) {
-        vector<int> coefficients;  // 存儲多項式的係數
+        vector<int> coeffs;  // 存儲多項式的係數
         int coeff;
         
         // 讀取係數，直到遇到換行字符
         while (cin >> coeff) {
-            coefficients.push_back(coeff);
+            coeffs.push_back(coeff);
             if(cin.peek() == '\n') 
                 break;
         }
 
         // 計算並輸出在 x 點的多項式導數
-        cout << calculateDerivative(x, coefficients) << endl;
+        cout << derivative(x, coeffs) << endl;
     }
 }
 
